3b8f00e2fcdfbbfacf5ae77961b9e31da85335f8
Kaique Lima <31413363+KaiqueLima3001@users.noreply.github.com>
2025-10-20 11:34:50 -0300

    Feat: Adicionando arquivos do projeto borda-se

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StatusBar,StyleSheet } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Login from './src/pages/login';
+import Routes from './src/routes/index.routes';
+import {themas} from './src/global/themes'
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor='#F5C518' barStyle="dark-content"/>
+      <Routes/>
+    </NavigationContainer>
+  );
+}
